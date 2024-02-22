c8c881e06d5711210ca9a93400e394d0b3bb24ef
Kappaessecci <katia_cannata@protonmail.com>
2024-02-22 15:02:41 +0100

    Player_movement_WASD

--- Script/Personaggio/movimento.gd
+++ Script/Personaggio/movimento.gd
@@ -3,5 +3,13 @@ extends Node2D
 func _ready():
 	pass 
 
-func _process(delta):
-	pass
+func _physics_process(delta):
+	if Input.is_action_pressed("move_right"):
+		move_local_x(+1)
+	if Input.is_action_pressed("move_left"):
+		move_local_x(-1)
+	if Input.is_action_pressed("move_forward"):
+		move_local_y(-1)
+	if Input.is_action_pressed("move_back"):
+		move_local_y(+1)
+
