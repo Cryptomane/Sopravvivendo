0bb39d5fef096c589bd5e8bb22ce754fa4bd89e6
Sergio Bonfiglio <bonfiglio.ser@gmail.com>
2024-03-29 11:54:37 +0100

    Tolto log

--- Script/nemico.gd
+++ Script/nemico.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var velocita_movimento = 100 # Velocità di movimento del nemico
+@export var velocita_movimento = 100 # Velocità di movimento del nemico
 var player : Node2D # Riferimento al giocatore
 
 func _ready():
@@ -10,8 +10,6 @@ func _ready():
 func _physics_process(delta):
 	# Calcolo la direzione verso il giocatore
 	var direzione = global_position.direction_to(player.global_position)
-	print("Player: [%d,%d] enemy: [%d,%d]" % [player.global_position.x, player.global_position.y, global_position.x, global_position.y])
-	
 	
 	# Applico la velocità alla direzione
 	velocity = direzione * velocita_movimento
